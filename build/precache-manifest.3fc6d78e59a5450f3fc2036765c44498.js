2669d024e48482c6b649e150e49c70bbc369dcdf
atocchi <andrewtocchi@gmail.com>
2020-01-30 13:21:17 -0800

    trying to server static file on its own

---
+++ build/precache-manifest.3fc6d78e59a5450f3fc2036765c44498.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "311ff1dd6067e49e293e34bc5af91349",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2cca247a474138be180",
+    "url": "/static/css/main.0cc373cf.chunk.css"
+  },
+  {
+    "revision": "7dd41a0c915c1e55b543",
+    "url": "/static/js/2.6a94be2e.chunk.js"
+  },
+  {
+    "revision": "d15f33d2758253ab987d61f794181995",
+    "url": "/static/js/2.6a94be2e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f2cca247a474138be180",
+    "url": "/static/js/main.31fcb9f8.chunk.js"
+  },
+  {
+    "revision": "784f934da7cae9e13356",
+    "url": "/static/js/runtime-main.1a63ad0f.js"
+  },
+  {
+    "revision": "b18200829d89f2aff4bdd736b6deb02d",
+    "url": "/static/media/beer.b1820082.png"
+  },
+  {
+    "revision": "e77939b9afd59c3bce07de7dab2c45ef",
+    "url": "/static/media/burg.e77939b9.png"
+  },
+  {
+    "revision": "1b7baaf55ab9758ee16620f3820d9647",
+    "url": "/static/media/cliff.1b7baaf5.jpg"
+  },
+  {
+    "revision": "4828bbf9853320a8f829455b03ee75ec",
+    "url": "/static/media/cliffbg.4828bbf9.jpg"
+  },
+  {
+    "revision": "af3df2335bfc25f5aa1dd286b7165ca7",
+    "url": "/static/media/fb.af3df233.png"
+  },
+  {
+    "revision": "17ab0fde600093afede95d8bec2b156c",
+    "url": "/static/media/friend.17ab0fde.png"
+  },
+  {
+    "revision": "e9d81012d123bb0c32a0ed8ba07a6869",
+    "url": "/static/media/hang.e9d81012.png"
+  },
+  {
+    "revision": "f4229ef70be6c9d5af31eafa69d2cc75",
+    "url": "/static/media/html.f4229ef7.png"
+  },
+  {
+    "revision": "2dda71ee27769f899e9d6b04189f083e",
+    "url": "/static/media/meLG.2dda71ee.jpg"
+  },
+  {
+    "revision": "ce912c3f11bfb78299ce7b9d9f31adca",
+    "url": "/static/media/node.ce912c3f.png"
+  },
+  {
+    "revision": "3a3eb75102a6b9e742744579e5c2cbfc",
+    "url": "/static/media/old.3a3eb751.png"
+  },
+  {
+    "revision": "6d5e3a321a35f3ff6bf93294c9556863",
+    "url": "/static/media/racket.6d5e3a32.png"
+  }
+]);
